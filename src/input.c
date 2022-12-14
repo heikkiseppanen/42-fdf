a1dce13866e36ef2ddaa4dff9b71bb8ca2f0aab5
Heikki Seppänen <6179953+heikkiseppanen@users.noreply.github.com>
2022-12-14 11:55:58 +0200

    added more linux keys

--- src/input.c
+++ src/input.c
@@ -6,7 +6,7 @@
 /*   By: hseppane <marvin@42.ft>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 14:27:03 by hseppane          #+#    #+#             */
-/*   Updated: 2022/12/13 14:16:59 by hseppane         ###   ########.fr       */
+/*   Updated: 2022/12/14 10:23:08 by hseppane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@
 int	key_hook(int keycode, void *param)
 {
 	t_app *const	app = param;
+	const float		rot_speed = 0.03;
 
 	if (keycode == KEY_ESCAPE)
 	{
@@ -33,15 +34,15 @@ int	key_hook(int keycode, void *param)
 	}
 	else if (keycode == KEY_LEFT)
 	{
-		if (app->gfx.rotation.z > -2 * M_PI)
+		if (app->gfx.rotation.z > 2 * M_PI)
 			app->gfx.rotation.z = 0;
-		app->gfx.rotation.z += 0.5;
+		app->gfx.rotation.z += rot_speed;
 	}
 	else if (keycode == KEY_RIGHT)
 	{
 		if (app->gfx.rotation.z < -2 * M_PI)
 			app->gfx.rotation.z = 0;
-		app->gfx.rotation.z -= 0.5;
+		app->gfx.rotation.z -= rot_speed;
 	}
 	return (1);
 }
