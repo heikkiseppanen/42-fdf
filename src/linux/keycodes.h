3f8ce21fd0a0ab6c28cccbab3c07023809d06b51
Heikki Seppänen <6179953+heikkiseppanen@users.noreply.github.com>
2022-12-17 21:00:06 +0200

    Added linux mouse buttons

--- src/linux/keycodes.h
+++ src/linux/keycodes.h
@@ -6,7 +6,7 @@
 /*   By: hseppane <marvin@42.ft>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 17:38:42 by hseppane          #+#    #+#             */
-/*   Updated: 2022/12/14 09:04:57 by hseppane         ###   ########.fr       */
+/*   Updated: 2022/12/17 20:55:34 by hseppane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,15 @@ enum e_key
 	KEY_ESCAPE = 0xFF1B,
 	KEY_LEFT = 0xFF51,
 	KEY_RIGHT = 0xFF53
+};
+
+enum	e_mouse_button
+{
+	MOUSE_LEFT = 1,
+	MOUSE_RIGHT = 3,
+	MOUSE_MID = 2,
+	SCROLL_DOWN = 5,
+	SCROLL_UP = 4,
 };
 
 #endif
