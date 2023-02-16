548af110242c4565134e654dfe14c1e8dceebc71
Heikki Seppanen <hseppane@c1r3p4.hive.fi>
2023-02-16 09:53:12 +0200

    fixed float3 rotation functions

--- src/float3_transform.c
+++ src/float3_transform.c
@@ -6,7 +6,7 @@
 /*   By: hseppane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 09:26:30 by hseppane          #+#    #+#             */
-/*   Updated: 2023/02/14 12:37:07 by hseppane         ###   ########.fr       */
+/*   Updated: 2023/02/16 09:51:33 by hseppane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,28 +36,34 @@ t_float3 float3_rot_x(t_float3 l, float rad)
 {
 	const float	dcos = cosf(rad);
 	const float	dsin = sinf(rad);
+	t_float3	result;
 
-	l.y = l.y * dcos + l.y * -dsin;
-	l.z = l.z * dsin + l.z * dcos;
-	return (l);
+	result.x = l.x;
+	result.y = (l.y * dcos) - (l.z * dsin);
+	result.z = (l.y * dsin) + (l.z * dcos);
+	return (result);
 }
 
 t_float3 float3_rot_y(t_float3 l, float rad)
 {
 	const float	dcos = cosf(rad);
 	const float	dsin = sinf(rad);
+	t_float3	result;
 
-	l.x = l.x * dcos + l.x * dsin;
-	l.z = l.z * -dsin + l.z * dcos;
-	return (l);
+	result.x = (l.x * dcos) + (l.z * dsin);
+	result.y = l.y;
+	result.z = -(l.x * dsin) + (l.z * dcos);
+	return (result);
 }
 
 t_float3 float3_rot_z(t_float3 l, float rad)
 {
 	const float	dcos = cosf(rad);
 	const float	dsin = sinf(rad);
+	t_float3	result;
 
-	l.x = l.x * dcos + l.x * -dsin;
-	l.y = l.y * dsin + l.y * dcos;
-	return (l);
+	result.x = (l.x * dcos) - (l.y * dsin);
+	result.y = (l.x * dsin) + (l.y * dcos);
+	result.z = l.z;
+	return (result);
 }
